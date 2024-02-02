e8098e3a3b29421888636aec02a22ea5651c49ab
Tabee <ayvanika@gmail.com>
2024-02-02 12:48:16 +0200

    yea more of ray interaction thing. jus a lil

--- Assets/Fucking Around/InteractTest.cs
+++ Assets/Fucking Around/InteractTest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractTest : MonoBehaviour, IInteractable, IEventRunner
+public class InteractTest : MonoBehaviour, IInteractable
 {
     public void Interact()
     {
@@ -10,9 +10,5 @@ public class InteractTest : MonoBehaviour, IInteractable, IEventRunner
         Debug.Log("I've been interacted with!");
         gameObject.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
 
-    }
-    public void RunEvent()
-    {
-            Debug.Log("Running event!");
     }
 }
--- Assets/Fucking Around/RayInteraction.cs
+++ Assets/Fucking Around/RayInteraction.cs
@@ -9,14 +9,21 @@ public class RayInteraction : MonoBehaviour
     [SerializeField] private float distance = 12f;
     [SerializeField] private bool isFromMouse = true;
 
-    //private Ray ray;
-    //private Vector3 pos;
-
     private System.Func<Ray> rayProvider;
 
-    //yoinked from chatgpt. delegate checks which option i chose and then uses that one.
+    private void OnEnable()
+    {
+        LoadInteractionOption();
+    }
     void Start()
     {
+        LoadInteractionOption();
+    }
+    void LoadInteractionOption()
+    {
+        //yoinked from chatgpt kinda. delegate checks which option i chose and then uses that one.
+        //whenever the bool is changed in Inspector and the script is re-enabled it updates how it's doing the interaction
+
         if (isFromMouse)
         {
             rayProvider = () => Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -33,23 +40,14 @@ public class RayInteraction : MonoBehaviour
     void Update()
     {
         //shoot a ray from either mouse position or object center (+1 above it) in the direction infront of it based on distance float, only hit the specified layer
-        //if (isFromMouse)
-        //{
-        //    ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //}
-        //else
-        //{
-        //    pos = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
-        //    ray = new Ray(pos, transform.forward);
-        //}
+        Ray ray = rayProvider.Invoke();
 
         //show the ray
-        Ray ray = rayProvider.Invoke();
         Debug.DrawRay(ray.origin, ray.direction * distance, isFromMouse ? Color.green : Color.red, 0.1f, false);
 
         if (Physics.Raycast(ray, out hitData, distance, interactionLayer))
             {
-            //if it hit something, run stuff
+            //if it hit something, tell me what it hit
             Debug.Log(hitData.collider.gameObject.name);
 
             //get the inherited interfaces from the hit object
@@ -61,7 +59,7 @@ public class RayInteraction : MonoBehaviour
                     if (Input.GetKeyDown(KeyCode.E))
                     {
                         Debug.Log("E pressed!");
-                        //use the method from the object, regardless of which script is using that method
+                        //use the inherited method from the object, regardless of which script is using that method
                         interactedObject.Interact();
                     }
                 }
